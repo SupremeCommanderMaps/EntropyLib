cd50251c822ce31475d5b9eb2bbaa0cc889f674a
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-12-12 23:17:26 +0100

    Use :AddDamageMod for buffDamage

--- src/UnitBuff.lua
+++ src/UnitBuff.lua
@@ -1,12 +1,13 @@
 local function round(x)
-    return x + 0.5 - (x + 0.5) % 1
+    return x
+    --return x + 0.5 - (x + 0.5) % 1
 end
 
 function buffDamage(unit, multiplier)
     for i = 1, unit:GetWeaponCount() do
         local wep = unit:GetWeapon(i)
         if wep.Label ~= 'DeathWeapon' and wep.Label ~= 'DeathImpact' then
-            wep:ChangeDamage(round(wep:GetBlueprint().Damage * multiplier))
+            wep:AddDamageMod(round(wep:GetBlueprint().Damage * (multiplier - 1)))
         end
     end
 end
