f0d11a7cdc9f080afdbe22dfda5fc05baf56ae61
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-12-12 23:45:42 +0100

    Add Formatter

---
+++ src/Formatter.lua
@@ -0,0 +1,32 @@
+local function remainder(x, divisor)
+    return x - math.floor(x / divisor) * divisor
+end
+
+local function isDivisibleBy(x, divisor)
+    return remainder(x, divisor) == 0
+end
+
+-- Does not format fractional percents
+function formatMultiplier(x)
+    if x > 3 and isDivisibleBy(x, 1) then
+        return x .. "x"
+    end
+
+    return math.floor(x * 100) .. "%"
+end
+
+function formatTime(seconds)
+    if isDivisibleBy(seconds, 60) then
+        return (seconds / 60) .. " minutes"
+    end
+
+    return seconds .. " seconds"
+end
+
+local function inTimeString(seconds)
+    if seconds == 0
+        return "immediately"
+    end
+
+    return "in " .. formatTime(seconds)
+end
