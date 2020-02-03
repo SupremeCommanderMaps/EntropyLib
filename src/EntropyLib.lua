046b96e273ac1a0b0b74c99a817278a730c44af3
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-02-03 21:31:43 +0100

    Finish initial Notifier

--- src/EntropyLib.lua
+++ src/EntropyLib.lua
@@ -1,28 +1,35 @@
 newInstance = function(libPath)
-
     local function libImport(fileName)
         return import(libPath .. 'src/' .. fileName)
     end
 
-    return {
-        newTextPrinter = function()
-            return libImport('TextPrinter.lua').newInstance()
-        end,
+    local this = {}
 
-        newUnitCreator = function()
-            return libImport('UnitCreator.lua').newUnitCreator()
-        end,
+    this.newTextPrinter = function()
+        return libImport('TextPrinter.lua').newInstance()
+    end
 
-        -- To prevent default spawning you need: ScenarioUtils.CreateResources = function() end
-        -- mapEditorTables is an import of the _tables.lua file created by the FAF map editor
-        -- playerArmyNamesToIndexMap is optional. Format: {ARMY_1 = 1, ARMY_2 = 2}
-        spawnAdaptiveResources = function(mapEditorTables, playerArmyNamesToIndexMap)
-            libImport('AdaptiveResources.lua').spawnResources(
-                libImport('ResourceCreator.lua').newInstance(),
-                mapEditorTables,
-                playerArmyNamesToIndexMap
-            )
-        end
-    }
+    this.newUnitCreator = function()
+        return libImport('UnitCreator.lua').newUnitCreator()
+    end
+
+    this.newNotifier = function()
+        return libImport('Notifier.lua').newInstance(
+            this.newTextPrinter()
+        )
+    end
+
+    -- To prevent default spawning you need: ScenarioUtils.CreateResources = function() end
+    -- mapEditorTables is an import of the _tables.lua file created by the FAF map editor
+    -- playerArmyNamesToIndexMap is optional. Format: {ARMY_1 = 1, ARMY_2 = 2}
+    this.spawnAdaptiveResources = function(mapEditorTables, playerArmyNamesToIndexMap)
+        libImport('AdaptiveResources.lua').spawnResources(
+            libImport('ResourceCreator.lua').newInstance(),
+            mapEditorTables,
+            playerArmyNamesToIndexMap
+        )
+    end
+
+    return this
 end
 
--- src/Notifier.lua
+++ src/Notifier.lua
@@ -1,24 +1,36 @@
-newInstance = function(textPrinter)
-    local function print(message, options)
-        options.location = "leftcenter"
-        textPrinter.print(string.rep(" ", 12) .. message, options)
+newInstance = function(textPrinter, defaultSize)
+    local function print(message, color, duration)
+        textPrinter.print(
+            string.rep(" ", 12) .. message,
+            {
+                color = color,
+                duration = duration or 4,
+                size = defaultSize or 30,
+                location = "leftcenter"
+            }
+        )
     end
 
-    local function warning(message)
-         print(message, {color = 'ffff5599'})
+    local function warning(message, durationInSeconds)
+        print(message, 'ffff5599', durationInSeconds)
     end
 
-    local function positive(message)
-         print(message, {color = 'ffff5599'})
+    local function positive(message, durationInSeconds)
+        print(message, 'ff55ff99', durationInSeconds)
     end
 
-    local function failure(message)
-         print(message, {color = 'ffff5599'})
+    local function failure(message, durationInSeconds)
+        print(message, 'ffff5555', durationInSeconds)
+    end
+
+    local function neutral(message, durationInSeconds)
+        print(message, "ffffffff", durationInSeconds)
     end
 
     return {
         warning = warning, -- ie Enemy units incoming
         positive = positive, -- ie Victory or Only one minute left
         failure = failure, -- ie Mission failure
+        message = neutral,
     }
 end
